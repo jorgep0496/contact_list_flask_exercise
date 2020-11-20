17d7b01b53b6c81c24a82a023a33ed17d3a1bb2a
Jorge Peña <jorgep0496@gmail.com>
2020-11-20 16:51:30 -0300

    Contact List with Flask

---
+++ README.md
@@ -0,0 +1,25 @@
+# CONTACT LIST API
+
+### 1) Create virtualenv
+
+    $ pipenv shell
+
+### 2) Install libraries
+
+    $ pipenv install
+
+### 3) Init migrations
+
+    $ pipenv run init
+
+### 4) Create migrations
+
+    $ pipenv run init
+
+### 5) Create tables on database
+
+    $ pipenv run upgrade
+
+### 6) Start Server
+
+    $ pipenv run start
---
+++ main.py
@@ -0,0 +1,116 @@
+from flask import Flask, render_template, request, jsonify
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+from models import db
+
+app = Flask(__name__)
+
+app.url_map.strict_slashes = False
+
+app.config['DEBUG'] = True
+app.config['ENV'] = 'development'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:Nanibole16@localhost:3306/contactlist'
+
+db.init_app(app)
+Migrate(app, db)
+manager = Manager(app)
+manager.add_command("db", MigrateCommand)
+
+@app.route('/')
+def main():
+    return render_template('index.html')
+
+@app.route('/apis/fake/contact/agenda', methods=['GET'])
+def all_agendas():
+    agendas = Contact.query.with_entities(Contact.agenda_slug).distinct()
+    agendas = list(map(lambda contact: contact.agenda_slug, agendas))
+    return jsonify(agenda_slug), 200
+
+@app.route('/apis/fake/contact/agenda/<agenda_slug>', methods=['GET', 'DELETE'])
+def all_contacts_by_agenda(agenda_slug):
+    if request.method == 'GET':
+        contacts = Contact.query.filter_by(agenda_slug=agenda_slug).all()
+        contacts = list(map(lambda contact: contact.serialize(), contacts))
+        return jsonify(contacts), 200
+
+    if request.method == 'DELETE':
+        contacts = Contact.query.filter_by(agenda_slug=agenda_slug).all()
+        for contact in contacts:
+            if contact:
+                contact.delete()
+
+        return jsonify({"success": "All contact deleted"}), 200
+
+@app.route('/apis/fake/contact/<int:contact_id>', methods=['GET', 'PUT', 'DELETE'])
+def contact(contact_id):
+    contact = Contact.query.filter_by(id=contact_id).first()
+    if request.method == 'GET':
+        if contact:
+            return jsonify(contact.serialize()), 200
+        else:
+            return jsonify({"msg": "No contact with that id found"}), 404
+    if request.method == 'DELETE':
+        if contact:
+            contact.delete()
+            return jsonify({"msg": f"Contact with {contact_id} has been deleted"}), 200
+        else:
+            return jsonify({"msg": "No contact with that id found"}), 404
+
+    if request.method == "PUT":
+        if contact:
+            full_name = request.json.get('full_name', None)
+            email = request.json.get('email', None)
+            agenda_slug = request.json.get('agenda_slug', None)
+            address = request.json.get('address', "")
+            phone = request.json.get('phone', "")
+
+            if not full_name:
+                return jsonify({"msg": "Full name is required"}), 400
+            if not email:
+                return jsonify({"msg": "Email required"}), 400
+            if not agenda_slug:
+                return jsonify({"msg": "Agenda slug is required"}), 400
+            
+            contact.full_name = full_name
+            contact.email = email
+            contact.agenda_slug = agenda_slug
+            contact.address = address
+            contact.phone = phone
+            contact.update()
+
+            return jsonify({"msg": f"Contact with id {contact_id} has been updated"}), 200
+
+        else:
+            return jsonify({"msg": "Id was not found in our database"}),  404
+
+@app.route('/apis/fake/contact', methods=['POST'])
+def create_contact():
+    full_name = request.json.get("full_name", None)
+    email = request.json.get("email", None)
+    agenda_slug = request.json.get("agenda_slug", None)
+    address = request.json.get("address", "")
+    phone = request.json.get("phone", "")
+
+    if not full_name:
+        return jsonify({"msg": "full_name is required"}), 400
+
+    if not email:
+        return jsonify({"msg": "email is required"}), 400
+
+    if not agenda_slug:
+        return jsonify({"msg": "agenda_slug is required"}), 400
+    
+    contact = Contact()
+    contact.full_name = full_name
+    contact.email = email
+    contact.agenda_slug = agenda_slug
+    contact.address = address
+    contact.phone = phone
+
+    contact.save()
+
+    return jsonify(contact.serialize()), 201
+
+if __name__ == '__main__':
+    manager.run()
